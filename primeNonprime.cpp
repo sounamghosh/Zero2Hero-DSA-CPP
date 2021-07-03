d39e1a6cbab5f23309c74597c5dbb50bac74263c
sounamghosh <sounambcrecedu@gmail.com>
2021-07-03 21:00:53 +0530

    made changes in comments

--- primeNonprime.cpp
+++ primeNonprime.cpp
@@ -7,11 +7,11 @@ int main() {
 	cin >> n;
 
 	int d = 2;
-	bool divided = false;
+	bool divided = false;                
 	while (d < n) {
 		if (n % d == 0) {
 			cout << "Not Prime" << endl; // here it print for every loop need to fix this.
-			divided = true;
+			divided = true;  
 		}
 		d = d + 1;
 	}
